ed97cd7d8164d09fd067b4b46bd7622b734efb8e
Travis Deane Ashworth <travisashworth2007@gmail.com>
2017-10-19 13:11:51 -0500

    Update server.jl

--- src/server.jl
+++ src/server.jl
@@ -1,4 +1,4 @@
-# Browser Window (Borrowed from Blink.jl)
+# Browser Window (Borrowed from Blink.jl)
 @static if is_apple()
     launch(x) = run(`open $x`)
 elseif is_linux()
@@ -12,13 +12,14 @@ const conditions = Dict{String,Condition}()
 conditions["connected"] = Condition()
 conditions["unloaded"] = Condition()
 
-Endpoint("/pages.js") do request::Request
-    readstring(joinpath(dirname(@__FILE__),"pages.js"))
+Endpoint("/Pages.js") do request::Request
+    readstring(joinpath(dirname(@__FILE__),"Pages.js"))
 end
 
 ws = WebSocketHandler() do request::Request, client::WebSocket
     while true
         msg = JSON.parse(String(read(client)))
+        print(msg)
         route = msg["route"]
         if !haskey(pages[route].sessions,client.id)
             pages[route].sessions[client.id] = client
@@ -29,12 +30,39 @@ end
 
 http = HttpHandler() do request::Request, response::Response
     route = URI(request.resource).path
-    if haskey(pages,route)
-        res = Response(pages[route].handler(request))
+    str = match(r"^(\/[\d\w- _]+)", route);
+    folder = String(str[1])
+
+    # We are in an existing folder
+    if haskey(pages, folder)
+        req = pages[folder].handler(request) # let's see what we're dealing with here.
+
+        # We will be serving from folder but is a file specified?
+        if length(folder) < length(route) # Yes, it is!
+            file = replace(route, r"^(\/[\d\w- _]+)", "");
+            println(file)
+
+        # No, no file specified so we will specify the standard default "/index.html"
+        else
+            file = "/index.html"
+        end
+
+        # I thought mimetypes should just work but found that I had to detect/set them
+        key = match(r"(?:\.(\w+$))", file)[1]
+        mime = mimetypes[ key ]
+
+        # Serve results
+        if length(Pages.pages[folder].folder) > 0
+            res = Response( 200, Dict{AbstractString,AbstractString}([("Content-Type", mime)]),
+                            readstring( Pages.pages[folder].folder * file ))
+        else
+            req # = "Page generated on server."
+        end
+
+    # Folder does not exist! ...Inform user.
     else
-        res = "Page not found."
+        res = "Sorry, page not found."
     end
-    res
 end
 
 server = Server(http,ws)
